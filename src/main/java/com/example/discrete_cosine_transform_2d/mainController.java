1a042767dd37eec0bf882ac136b978224e85209d
RioSamson <76279598+RioSamson@users.noreply.github.com>
2023-12-07 15:12:52 -0800

    Initial commit

---
+++ src/main/java/com/example/discrete_cosine_transform_2d/mainController.java
@@ -0,0 +1,10 @@
+package com.example.discrete_cosine_transform_2d;
+
+import javafx.fxml.FXML;
+
+public class mainController {
+
+    @FXML
+    protected void openButtonClicked() {
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.discrete_cosing_transform_2d {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.discrete_cosine_transform_2d to javafx.fxml;
+    exports com.example.discrete_cosine_transform_2d;
+}
